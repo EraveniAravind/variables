2dde12d51462dabeba088b8b56fd9afa5b8f38ba
EraveniAravind <eraveniaravind6038@gmail.com>
2024-10-20 00:03:36 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+var a=2;
+var b=3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b)
+let c=4;
+let d=6;
+console.log(c/d);
+console.log(c+d);
+console.log(c-d);
+console.log(c*d);
+const z=34;
+const y=45;
+console.log(z+y);
+console.log(z-y);
+console.log(z*y);
+console.log(z/y)
